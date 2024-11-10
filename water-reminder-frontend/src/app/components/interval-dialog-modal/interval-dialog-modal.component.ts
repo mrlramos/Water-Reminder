68c2633c18ec5daf46576d172efc251d67a796db
Marlon Ramos <marlon_raoni10@hotmail.com>
2024-11-10 10:58:52 -0300

    Add CommonModule to interval-dialog-modal.component

--- water-reminder-frontend/src/app/components/interval-dialog-modal/interval-dialog-modal.component.ts
+++ water-reminder-frontend/src/app/components/interval-dialog-modal/interval-dialog-modal.component.ts
@@ -5,6 +5,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-interval-dialog-modal',
@@ -16,6 +17,7 @@ import { MatIconModule } from '@angular/material/icon';
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    CommonModule
   ],
   templateUrl: './interval-dialog-modal.component.html',
   styleUrls: ['./interval-dialog-modal.component.scss'],
